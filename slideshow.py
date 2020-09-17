fe18f18cf235e994dc64e489b2fefcd7f5d74e13
Jiun Kim <jkim2@bowdoin.edu>
2020-09-17 23:35:34 +0900

    organize

--- slideshow.py
+++ slideshow.py
@@ -1,17 +1,11 @@
 import tkinter as tk
 from PIL import Image, ImageTk
-import time
-import sys
-import os
+from start import get_sequence_of_yoga_moves
 
 class HiddenRoot(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        #hackish way, essentially makes root window
-        #as small as possible but still "focused"
-        #enabling us to use the binding on <esc>
         self.wm_geometry("0x0+0+0")
-
         self.window = MySlideShow(self)
         self.window.startSlideShow()
 
@@ -19,7 +13,7 @@ class HiddenRoot(tk.Tk):
 class MySlideShow(tk.Toplevel):
     def __init__(self, *args, **kwargs):
         tk.Toplevel.__init__(self, *args, **kwargs)
-        #remove window decorations 
+        #remove window decorations
         self.overrideredirect(True)
 
         #save reference to photo so that garbage collection
@@ -27,6 +21,9 @@ class MySlideShow(tk.Toplevel):
         self.persistent_image = None
         self.imageList = []
         self.pixNum = 0
+
+        self.width = 600
+        self.height = 450
 
         #used to display as background image
         self.label = tk.Label(self)
@@ -38,17 +35,11 @@ class MySlideShow(tk.Toplevel):
         '''
         Get image directory from command line or use current directory
         '''
-        if len(sys.argv) == 2:
-            curr_dir = sys.argv[1]
-        else:
-            curr_dir = '.'
-
-        for root, dirs, files in os.walk(curr_dir):
-            for f in files:
-                if f.endswith(".png") or f.endswith(".jpg"):
-                    img_path = os.path.join(root, f)
-                    print(img_path)
-                    self.imageList.append(img_path)
+        images = get_sequence_of_yoga_moves(10)
+        for i in range(0, len(images)-1):
+            print(images[i])
+            self.imageList.append("./yoga_pose/" + images[i] + ".jpg")
+        return images
 
     def startSlideShow(self, delay=4): #delay in seconds
         myimage = self.imageList[self.pixNum]
@@ -58,19 +49,15 @@ class MySlideShow(tk.Toplevel):
         self.after(delay*1000, self.startSlideShow)
 
     def showImage(self, filename):
-        image = Image.open(filename)  
-
-        img_w, img_h = image.size
-        scr_w, scr_h = self.winfo_screenwidth(), self.winfo_screenheight()
-        width, height = min(scr_w, img_w), min(scr_h, img_h)
-        image.thumbnail((width, height), Image.ANTIALIAS)
+        image = Image.open(filename)
+        image.thumbnail((self.width, self.height), Image.ANTIALIAS)
 
         #set window size after scaling the original image up/down to fit screen
         #removes the border on the image
         scaled_w, scaled_h = image.size
         self.wm_geometry("{}x{}+{}+{}".format(scaled_w,scaled_h,0,0))
-        
-        # create new image 
+
+        # create new image
         self.persistent_image = ImageTk.PhotoImage(image)
         self.label.configure(image=self.persistent_image)
 
